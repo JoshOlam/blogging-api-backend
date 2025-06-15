ef4cb5a6959216e30f5e80f6e38d9c095cca9880
Josh <90909849+JoshOlam@users.noreply.github.com>
2025-06-15 20:44:20 +0100

    Added deployed mongodb instance

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const clientOptions = {
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    // console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
     console.log('MongoDB connected...');
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
